01c95fea762892b3d837dc5fa8d0f3a83f3e5ccd
FurizAlex <55350194+Alexchin714@users.noreply.github.com>
2024-11-21 22:57:52 +0800

    Delete Blind Robot directory
    delete

--- Blind Robot/Intros/Scripts/Intro.gd
+++
@@ -1,8 +0,0 @@
-extends CanvasLayer
-
-func _ready():
-	Input.set_mouse_mode(Input.MOUSE_MODE_HIDDEN)
-	
-func _on_Timer_timeout():
-	SceneTransition.change_scene("res://Scene/TitleScreen.tscn")
-	Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
