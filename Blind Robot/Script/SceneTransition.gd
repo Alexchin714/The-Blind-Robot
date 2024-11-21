753c8973edb178772142e30e46c95828135fa45f
FurizAlex <55350194+Alexchin714@users.noreply.github.com>
2024-11-21 22:57:52 +0800

    Delete Blind Robot directory
    delete

--- Blind Robot/Script/SceneTransition.gd
+++
@@ -1,7 +0,0 @@
-extends CanvasLayer
-	
-func change_scene(target: String) -> void:
-	$AnimationPlayer.play("SceneChanged")
-	yield($AnimationPlayer,'animation_finished')
-	get_tree().change_scene(target)
-	$AnimationPlayer.play_backwards("SceneChanged")
